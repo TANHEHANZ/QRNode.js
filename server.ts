af0d87491fd077810e94f38cf358db6c9a35c552
TANHEHANZ <hantach10@gmail.com>
2025-03-11 17:59:48 +0100

    inicializacion de proyectoç

---
+++ server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/prueba", (req, res) => {
+  res.send("¡Hola Mundo con Node.js y Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});

f0c3aa47912fe045c63c7b10d7f5ccbbdd3857be
TANHEHANZ <hantach10@gmail.com>
2025-03-13 17:43:57 +0100

    configuracion de controlado

--- controllerPDF.ts
+++ controllerPDF.ts
@@ -23,27 +23,21 @@ export const pdf = async (req: Request, res: Response) => {
     );
     const pdfBuffer = Buffer.from(pdfBase64, "base64");
     const pdfDoc = await PDFLib.load(pdfBuffer);
-
-    // Convert QR code to PNG format
     const qrImageBytes = Buffer.from(qrImage.split(",")[1], "base64");
     const qrImageEmbed = await pdfDoc.embedPng(qrImageBytes);
 
-    // Add QR code to each page
     const pages = pdfDoc.getPages();
     const currentDate = new Date().toLocaleString();
     pages.forEach((page) => {
-      const { width, height } = page.getSize();
+      const { height } = page.getSize();
       const qrWidth = widthQR.w || 100;
       const qrHeight = widthQR.h || 100;
-
-      // Draw QR code
       page.drawImage(qrImageEmbed, {
         x: position.x,
         y: height - position.y - qrHeight,
         width: qrWidth,
         height: qrHeight,
       });
-
       page.drawText(selloText, {
         x: position.x + 30,
         y: height - position.y + 10,
@@ -52,11 +46,9 @@ export const pdf = async (req: Request, res: Response) => {
         rotate: degrees(90),
         maxWidth: 180,
       });
-
       page.drawText(`Generado: ${currentDate}`, {
         x: position.x + qrWidth - 20,
         y: height - position.y + 10,
-
         size: 8,
         color: rgb(0, 0, 0),
         rotate: degrees(90),
@@ -64,7 +56,6 @@ export const pdf = async (req: Request, res: Response) => {
       });
     });
     const modifiedPdfBytes = await pdfDoc.save();
-
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader(
       "Content-Disposition",
